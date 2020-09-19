3a502da45af4c3d179e15ee1f408bc8f052abf66
Robert Kempton <r@snow87.com>
2020-09-19 02:06:40 -0600

    use env file format that works with docker

--- www.js
+++ www.js
@@ -2,7 +2,13 @@
 const fs = require( 'fs' )
 let envFile = require( 'os' ).homedir() + '/.dinner-at-home.env'
 if( fs.existsSync( envFile ) ) {
-    const appEnv = require( envFile )
+    const appEnv = fs.readFileSync( envFile ).toString('utf-8').split( '\n' )
+                         .map( line => line.split( /=(.+)/ ) )
+                         .reduce( ( res, kv ) => {
+                      res[ kv[ 0 ] ] = kv[ 1 ]
+                      return res
+                  }, {} )
+
     Object.assign( process.env, appEnv )
 }
 
